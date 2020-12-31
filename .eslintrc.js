ef5765ef30f55ab4c48b5b664f7bc52257fb9bc3
Salim Erradi <erradisalim@gmail.com>
2020-12-31 14:49:00 -0500

    fix: eslintrc.js changed file to run AIA properly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
-    browser: true,
+    node: true
   },
-  plugins: ["security", "prettier"],
+  plugins: ["security"],
   extends: [
-    "eslint:recommended",
     "plugin:security/recommended",
-    "plugin:prettier/recommended",
+    "plugin:vue/essential",
+    "@vue/prettier",
+    "@vue/typescript"
   ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
-  overrides: [
-    {
-      files: ["**/*.ts"],
-      parser: "@typescript-eslint/parser",
-    },
-    {
-      files: ["**/*.vue"],
-      parser: "vue-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-      },
-      extends: ["plugin:vue/recommended", "@vue/typescript", "@vue/prettier"],
-    },
-  ],
+  parserOptions: {
+    parser: "@typescript-eslint/parser"
+  }
 };
+
